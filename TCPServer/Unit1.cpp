850e50b6936714e9d97b9fdc2679134531e4f435
techhouse_150908 <kmaehara@mel.co.jp>
2019-09-27 11:47:34 +0900

    さらに修正

--- TCPServer/Unit1.cpp
+++ TCPServer/Unit1.cpp
@@ -91,7 +91,9 @@ void __fastcall TTCP_Server::ChatServerExcute(TIdContext *AContext)
 
 				if (i==StringGrid1->RowCount-1) {
 					StringGrid1->RowCount += 1;
+					StringGrid1->Cells[0][StringGrid1->RowCount-1] = StringGrid1->RowCount-1;
 					StringGrid1->Cells[5][StringGrid1->RowCount-1] = AContext->Connection->Socket->Binding->PeerPort;
+					StringGrid1->Cells[4][StringGrid1->RowCount-1] = AContext->Connection->Socket->Binding->PeerIP;
 				}
 
 			}
